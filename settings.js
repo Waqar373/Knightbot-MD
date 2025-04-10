e6830e3dea1dfa25937240bbe0007b94a98fe238
Waqar373 <ws4476947@gmail.com>
2025-04-11 01:37:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'MAJNU',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "MR MAJNU",
+  botOwner: 'majnu', // Your name
+  ownerNumber: '923113213996', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
